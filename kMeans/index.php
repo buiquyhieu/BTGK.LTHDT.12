21f4020bc02be49817ef04e9a7abe3d10c65692f
buiquyhieu <buiquyhieu1995@gmail.com>
2017-05-16 22:58:17 +0700

    Add files via upload

---
+++ kMeans/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <style>
+            div{
+                position: absolute;
+                width: 10px;
+                height: 10px;
+                /*background-color: green;*/
+            }
+        </style>
+    </head>
+    <body style="position: relative">
+        <div id="result" style="top:20px;left: 20px"></div>
+        <button id="btn">Click !</button>
+<!--        <script>
+            for (var i = 0; i < 10; i++) {
+                var div = document.createElement("div");
+                div.style.top = 10 * i + "px";
+                div.style.left = 10 * i + "px";
+                div.style.backgroundColor = "rgb(200,150," + 30 * i + ")";
+                document.body.appendChild(div);
+            }
+        </script>-->
+        <script>
+            var mang = new Array();
+            $('#btn').click(function () {
+                $.ajax({
+                    url: 'kmean.php',
+                    type: 'post',
+                    dataType: 'json',
+                    success: function (result) {
+                        console.log(result);
+
+                        var html = '';
+                        html += '<table border="1" cellpadding="10" style="color:blue" >';
+                        html += '<tr><th>Toán</th><th>Văn</th><th>Nhóm</th></tr>';
+                        $.each(result, function (key, item) {
+                            mang.push(item);
+                        });
+                        for (var i = 0; i < mang.length; i++) {
+                            for (var j = 0; j < mang[i]['tamcum'].nhom; j++) {
+                                html += '<tr>';
+                                html += '<td>' + mang[i][j].toan + '</td>';
+                                html += '<td>' + mang[i][j].van + '</td>';
+                                html += '<td>' + mang[i][j].nhom + '</td>';
+                                html += '</tr>';
+                            }
+                        }
+
+//                        console.log(mang[0][0].toan);
+                        html += '</table>';
+                        $('#result').html(html);
+                        
+                        for (var i = 0; i < mang.length; i++) {
+                            for (var j = 0; j < mang[i]['tamcum'].nhom; j++) {
+                                var div = document.createElement("div");
+                                var xleft=mang[i][j].van * 100;
+                                var ytop=mang[i][j].toan * 100;
+                                var mau=mang[i][j].nhom * 50;
+                                div.style.top = ytop + "px";
+                                div.style.left = xleft + "px";
+                                div.style.backgroundColor = "rgb(100,"+parseFloat(mau*i*2)+"," + mau + ")";
+                                document.body.appendChild(div);
+                            }
+                            var div = document.createElement("div");
+                                div.style.top = mang[i]['tamcum'].toan * 100 + "px";
+                                div.style.left = mang[i]['tamcum'].van * 100 + "px";
+                                div.style.backgroundColor = "rgb(255,0,0)";
+                                document.body.appendChild(div);
+                        }
+                    }
+                });
+            });
+        </script>
+
+        <?php
+        ?>
+    </body>
+</html>
---
+++ kMeans/kmean.php
@@ -0,0 +1,126 @@
+<?php
+
+$dulieu = array(
+            0 => array(
+                'toan' => 5.5,
+                'van' => 4.5,
+                'nhom' => NULL
+            ),
+            1 => array(
+                'toan' => 4,
+                'van' => 4.25,
+                'nhom' => NULL
+            ),
+            2 => array(
+                'toan' => 4,
+                'van' => 4.5,
+                'nhom' => NULL
+            ),
+            3 => array(
+                'toan' => 6,
+                'van' => 5.75,
+                'nhom' => NULL
+            ),
+            4 => array(
+                'toan' => 6,
+                'van' => 5.25,
+                'nhom' => NULL
+            ),
+            5 => array(
+                'toan' => 6,
+                'van' => 5.25,
+                'nhom' => NULL
+            ),
+            6 => array(
+                'toan' => 4,
+                'van' => 6,
+                'nhom' => NULL
+            ),
+            7 => array(
+                'toan' => 4.5,
+                'van' => 4,
+                'nhom' => NULL
+            ),
+            8 => array(
+                'toan' => 3,
+                'van' => 5.25,
+                'nhom' => NULL),
+            9 => array(
+                'toan' => 2,
+                'van' => 3.25,
+                'nhom' => NULL
+            )
+        );
+        $N = count($dulieu);
+//        echo $N;
+        $cum = array(
+            1 => array(
+                'tamcum' => array()
+            ),
+            2 => array(
+                'tamcum' => array()
+            )
+        );
+        $cum[1]['tamcum'] = $dulieu[0];
+        $cum[2]['tamcum'] = $dulieu[1];
+        $temp = 1000;
+        $t = 0;
+        do {
+//    $t++;
+            for ($i = 1; $i < 3; $i++) {
+                $cum[$i]['tamcum']['nhom'] = 0;
+            }
+            for ($i = 0; $i < $N; $i++) {
+                $min = 1000;
+                for ($j = 1; $j < 3; $j++) {
+                    $khoangcach = pow($dulieu[$i]['toan'] - $cum[$j]['tamcum']['toan'], 2) + pow($dulieu[$i]['van'] - $cum[$j]['tamcum']['van'], 2);
+                    if ($khoangcach < $min) {
+                        $min = $khoangcach;
+                        $dulieu[$i]['nhom'] = $j;
+                    }
+                }
+                $abc = $cum[$dulieu[$i]['nhom']]['tamcum']['nhom'];
+                $cum[$dulieu[$i]['nhom']]['tamcum']['nhom'] = $cum[$dulieu[$i]['nhom']]['tamcum']['nhom'] + 1;
+
+                $cum[$dulieu[$i]['nhom']][$abc] = $dulieu[$i];
+            }
+//        if ($t==5) {
+//            echo '<pre>';
+//            print_r($cum);
+//            break;
+//        }
+            $tongKC = 0;
+//        echo $cum[1][0]['toan'];
+            for ($i = 1; $i < 3; $i++) {
+                $m = count($cum[$i]) - 1;
+                for ($j = 0; $j < $m; $j++) {
+//               $kc=sqrt(pow($cum[$i]['tamcum']['toan']-$cum[$i][$j]['toan'], 2)+pow($cum[$i]['tamcum']['van']-$cum[$i][$j]['van'], 2));
+//               echo $kc.'<br/>';
+                    $tongKC = $tongKC + sqrt(pow($cum[$i]['tamcum']['toan'] - $cum[$i][$j]['toan'], 2) + pow($cum[$i]['tamcum']['van'] - $cum[$i][$j]['van'], 2));
+                }
+            }
+//       echo $tongKC;
+            if (abs($tongKC - $temp) < 0.00001) {
+                die(json_encode($cum));
+        
+                break;
+            } else {
+                $temp = $tongKC;
+            }
+//        echo '<pre>';
+//        print_r($cum);
+            for ($i = 1; $i < 3; $i++) {
+                $tongToan = 0;
+                $tongVan = 0;
+                $soluong = count($cum[$i]) - 1;
+                for ($j = 0; $j < $soluong; $j++) {
+                    $tongToan = $tongToan + $cum[$i][$j]['toan'];
+                    $tongVan = $tongVan + $cum[$i][$j]['van'];
+                }
+                $cum[$i]['tamcum']['toan'] = $tongToan / $soluong;
+                $cum[$i]['tamcum']['van'] = $tongVan / $soluong;
+                for ($k = 0; $k < $soluong; $k++) {
+                    unset($cum[$i][$k]);
+                }
+            }
+        } while (true);
